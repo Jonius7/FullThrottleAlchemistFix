4328851bf95055c2bc11da1bd2a22193f12d7e1e
Jonius7 <jhgenius01@hotmail.com>
2025-09-01 13:07:25 +1000

    Shim attempt 1 to remove outdated Forestry API

--- src/main/java/jonius7/ftafix/FtaFixMod.java
+++ src/main/java/jonius7/ftafix/FtaFixMod.java
@@ -1,26 +1,5 @@
 package jonius7.ftafix;
 
-import cpw.mods.fml.common.Mod;
-import cpw.mods.fml.common.Mod.EventHandler;
-import cpw.mods.fml.common.event.FMLInitializationEvent;
-import cpw.mods.fml.common.event.FMLPostInitializationEvent;
-import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+public class FtaFixMod {
 
-@Mod(modid = FtaFixMod.MODID, name = FtaFixMod.NAME, version = FtaFixMod.VERSION)
-public class FtaFixMod{
-	
-	public static final String MODID = "ftafix";
-	public static final String NAME = "FtaFix";
-    public static final String VERSION = "1.0";
-	
-	@EventHandler
-	public void preInit(FMLPreInitializationEvent event) {
-	}
-	@EventHandler
-	public void init(FMLInitializationEvent event) {
-		System.out.println("[FtaFix] Initialised");
-	}
-	  
-	@EventHandler
-	public void postInit(FMLPostInitializationEvent event) {}
 }
