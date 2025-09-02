ec02b63d2caafe53b2c881adbff39b1d7b292f82
Jonius7 <jhgenius01@hotmail.com>
2025-09-03 01:50:47 +1000

    Clean up FtaFixCore

--- src/main/java/jonius7/ftafix/core/FtaFixCore.java
+++ src/main/java/jonius7/ftafix/core/FtaFixCore.java
@@ -1,58 +1,37 @@
 package jonius7.ftafix.core;
 
 import cpw.mods.fml.relauncher.IFMLLoadingPlugin;
-import cpw.mods.fml.relauncher.IFMLLoadingPlugin.MCVersion;
 
-import java.io.*;
+import java.io.File;
 import java.util.Map;
 
-@MCVersion("1.7.10")
+@IFMLLoadingPlugin.MCVersion("1.7.10")
 public class FtaFixCore implements IFMLLoadingPlugin {
 
-    private static final File MARKER_FILE = new File("config/FTA_PATCHED.marker");
-
     public FtaFixCore() {
-        if (!MARKER_FILE.exists()) {
-            boolean patched = patchFullThrottleAlchemist();
-            if (patched) writeMarker();
-        }
+        patchFullThrottleAlchemist();
     }
 
-    private boolean patchFullThrottleAlchemist() {
+    private void patchFullThrottleAlchemist() {
         try {
             File modsDir = new File("mods");
-            File target = new File(modsDir, "FullThrottle_Alchemist-1.7.10-1.0.18a.jar");
-            File patched = new File(modsDir, "FullThrottle_Alchemist-1.7.10-1.0.18a-patched.jar");
-            File backup = new File(modsDir, "FullThrottle_Alchemist-1.7.10-1.0.18a.jar.bak");
+            File originalJar = new File(modsDir, "FullThrottle_Alchemist-1.7.10-1.0.18a.jar");
+            File patchedJar = new File(modsDir, "FullThrottle_Alchemist-1.7.10-1.0.18a-patched.jar");
+            File backupJar = new File(modsDir, "FullThrottle_Alchemist-1.7.10-1.0.18a.jar.bak");
 
-            if (target.exists() && !patched.exists()) {
-                System.out.println("[FTA Fix] Found FTA jar, patching...");
-                JarPatcher.stripForestry(target, patched);
+            if (originalJar.exists() && !patchedJar.exists()) {
+                System.out.println("[FTA Fix] Found FullThrottle Alchemist jar, patching...");
+                JarPatcher.stripForestry(originalJar, patchedJar);
 
-                if (target.renameTo(backup)) {
-                    System.out.println("[FTA Fix] Original jar renamed to: " + backup.getName());
+                if (originalJar.renameTo(backupJar)) {
+                    System.out.println("[FTA Fix] Original jar renamed to: " + backupJar.getName());
                 } else {
                     System.out.println("[FTA Fix] WARNING: Could not rename original jar.");
-                    return false;
                 }
 
-                System.out.println("[FTA Fix] Patched jar created: " + patched.getName());
+                System.out.println("[FTA Fix] Patched jar created: " + patchedJar.getName());
             }
         } catch (Exception e) {
-            e.printStackTrace();
-            return false;
-        }
-        return true;
-    }
-
-    private void writeMarker() {
-        try {
-            File dir = MARKER_FILE.getParentFile();
-            if (!dir.exists()) dir.mkdirs();
-            try (FileWriter fw = new FileWriter(MARKER_FILE)) {
-                fw.write("Patched FullThrottle Alchemist\n");
-            }
-        } catch (IOException e) {
             e.printStackTrace();
         }
     }
